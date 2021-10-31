12a2494e860a3a54266670bafd7243aaaaf5498c
Kraylor <gameking.mohm@hotmail.com>
2021-10-31 21:58:37 +0100

    removed unused import

--- app/src/main/java/com/test/planda/services/DatabaseService.java
+++ app/src/main/java/com/test/planda/services/DatabaseService.java
@@ -1,6 +1,9 @@
 package com.test.planda.services;
 
 import android.util.Log;
+
+import androidx.annotation.StringRes;
+
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
